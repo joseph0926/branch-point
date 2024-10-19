864e2b71f34e14350d24e469af7b65675b6c7677
joseph0926 <joseph0926@naver.com>
2024-10-19 14:15:51 +0900

    chore: next-ui 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { NextUIProvider } from "@/providers/next-ui.provider";
 
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
@@ -20,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${pretendard.className} antialiased`}>{children}</body>
+      <body className={`${pretendard.className} antialiased`}>
+        <NextUIProvider>{children}</NextUIProvider>
+      </body>
     </html>
   );
 }
